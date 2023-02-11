76e56803f1e4c6fbe6daf73267a8a5a320f9a027
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-11 13:25:57 +0200

    basic table

--- DataBase.py
+++ DataBase.py
@@ -5,4 +5,14 @@ conn = sqlite3.connect('employee.db')
 
 c = conn.cursor()
 
-c.execute("""CREATE TABLE employees""")
+# c.execute("""CREATE TABLE employees (
+ #       first text,
+  #      last text,
+   #     pay integer
+    #    )""")
+
+
+
+conn.commit()
+
+conn.close()

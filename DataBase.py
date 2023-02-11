eed266a93d00326bba8ba48fe791d7c6a631ca95
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-11 15:09:03 +0200

    a

--- DataBase.py
+++ DataBase.py
@@ -15,19 +15,15 @@ Employee1 = Employee('Gogo', 'Petrov', 2000)
 
 Employee2 = Employee('Vasko', 'Petrov', 3000)
 
-print(Employee1.first)
-print(Employee1.last)
-print(Employee1.pay)
+#c.execute("INSERT INTO employees VALUES (?, ?, ?)", (Employee1.first, Employee1.last, Employee1.pay))
+#conn.commit()
 
-c.execute("INSERT INTO employees VALUES (?, ?, ?)", (Employee1.first, Employee1.last, Employee1.pay))
-conn.commit()
-
-c.execute("INSERT INTO employees VALUES (:first, :last, :pay)", {'first': Employee2.first, 'last':Employee2.last, 'pay':Employee2.pay})
-conn.commit()
+#c.execute("INSERT INTO employees VALUES (:first, :last, :pay)", {'first': Employee2.first, 'last':Employee2.last, 'pay':Employee2.pay})
+#conn.commit()
 
 c.execute("SELECT * FROM employees WHERE last='Nakov'")
 
-print(c.fetchmany(2))
+print(c.fetchall())
 
 conn.commit()
 
